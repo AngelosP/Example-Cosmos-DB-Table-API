f2fb3da360c7b973e97dbd21ede8176e680d0b2c
Angelos Petropoulos <angelpe@microsoft.com>
2020-06-13 01:22:21 -0500

    Add project files.

---
+++ CustomerEntity.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example_Cosmos_DB_Table_API
+{
+    public class CustomerEntity : TableEntity
+    {
+        public CustomerEntity()
+        {
+        }
+
+        public CustomerEntity(string lastName, string firstName)
+        {
+            PartitionKey = lastName;
+            RowKey = firstName;
+        }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Threading.Tasks;
+
+namespace Example_Cosmos_DB_Table_API
+{
+    class Program
+    {
+        public static string ConnectionString = "DefaultEndpointsProtocol=https;AccountName=[AccountName];AccountKey=[AccountKey]";
+
+        static void Main(string[] args)
+        {
+            Run().Wait();
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+
+        public static async Task Run()
+        {
+            // Create a new table
+            CloudTable table = await CreateTableAsync("Customers");
+
+            try
+            {
+                // Create a new record in the table
+                CustomerEntity customer = new CustomerEntity("Angelos", "Petropoulos");
+                TableOperation insertOrMergeOperation = TableOperation.InsertOrMerge(customer);
+                TableResult result = await table.ExecuteAsync(insertOrMergeOperation);
+                customer = result.Result as CustomerEntity;
+
+                // Delete the newly created record
+                TableOperation deleteOperation = TableOperation.Delete(customer);
+                await table.ExecuteAsync(deleteOperation);
+
+            }
+            finally
+            {
+                // Delete the table itself
+                await table.DeleteIfExistsAsync();
+            }
+        }
+
+        public static async Task<CloudTable> CreateTableAsync(string tableName)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConnectionString);
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable table = tableClient.GetTableReference(tableName);
+            await table.CreateIfNotExistsAsync();
+            return table;
+        }
+    }
+}
